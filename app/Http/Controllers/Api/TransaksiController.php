c39c40e0386cf4f1f8125c3aef5f42b421b7dbfe
galihwicakshm <galihwicaksonomukti2000@gmail.com>
2022-10-30 23:14:34 +0700

    fix transaksi cart over qty

--- app/Http/Controllers/Api/TransaksiController.php
+++ app/Http/Controllers/Api/TransaksiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Cart;
 use App\Models\Barang;
 use App\Models\Transaksi;
 use App\Models\DetailOrder;
@@ -53,14 +54,78 @@ class TransaksiController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    // public function store(Request $request)
+    // {
+
+    //     $id_user = auth()->user()->id_user;
+
+    //     $validatorTransaksi = Validator::make($transaksi = $request->all(), [
+    //         'no_order' => ['required'],
+    //         'tanggal_order' => ['required'],
+    //         'nama_penerima' => ['required'],
+    //         'alamat' => ['required'],
+    //         'telp_penerima' => ['required'],
+    //         'total_berat' => ['required'],
+    //         'ongkir' => ['required'],
+    //         'total_bayar' => ['required'],
+    //         'status_bayar' => ['required'],
+    //     ]);
+
+    //     $validatorDetail = Validator::make($detail = $request->all(), [
+    //         'no_order' => ['required'],
+
+
+    //     ]);
+
+    //     if ($validatorDetail->fails() && $validatorTransaksi->fails()) {
+    //         return response()->json(['status' => 400, 'errors' => $validatorTransaksi->errors(), 'detail_order' => $validatorDetail->errors()], 400);
+    //     } else if ($validatorDetail->fails()) {
+    //         return response()->json(['status' => 400, 'errors' => $validatorDetail->errors()], 400);
+    //     } else if ($validatorTransaksi->fails()) {
+    //         return response()->json(['status' => 400, 'errors' => $validatorTransaksi->errors()], 400);
+    //     }
+
+    //     try {
+
+    //         $barang = Transaksi::create([
+    //             'id_user' => $id_user,
+    //             'no_order' => $request->no_order,
+    //             'tanggal_order' => $request->tanggal_order,
+    //             'nama_penerima' => $request->nama_penerima,
+    //             'alamat' => $request->alamat,
+    //             'ongkir' => $request->ongkir,
+    //             'telp_penerima' => $request->telp_penerima,
+    //             'total_berat' => $request->total_berat,
+    //             'total_bayar' => $request->total_bayar,
+    //             'status_bayar' => $request->status_bayar,
+    //         ]);
+
+    //         $detail = DetailOrder::create([
+    //             'no_order' => $request->no_order,
+    //             'id_user' =>  $id_user,
+    //         ]);
+    //         $qtybarang = Barang::find($request->id_barang);
+    //         $qtybarangs = DB::table('barangs')->where('id_barang', $request->id_barang);
+    //         $qtyminus = ($qtybarang['stok'] - $request->qty);
+    //         $qtybarangs->update([
+    //             'stok' => $qtyminus
+    //         ]);
+    //         return response()->json(['status' => 200, 'meesage' => 'Transaksi berhasil ditambahkan', 'data'  =>  $barang, 'detail_order' => $detail], 200);
+    //     } catch (\Exception $e) {
+    //         return response()->json(['status' => 400, 'errors' => $e->getMessage()], 400);
+    //     }
+    // }
+
+
     public function store(Request $request)
     {
-
         $id_user = auth()->user()->id_user;
-
-        // var_dump(auth()->user()->id_user);
+        $cart = Cart::join('barangs', 'barangs.id_barang', '=', 'carts.id_barang')->where('id_user', $id_user)->get();
+        $cartz = Cart::join('barangs', 'barangs.id_barang', '=', 'carts.id_barang')->where('id_user', $id_user)->get();
+        $checkcart = Cart::select('qty')->where('id_user', $id_user)->get();
+        $lebih = Cart::join('barangs', 'barangs.id_barang', '=', 'carts.id_barang')->select('qty')->where('id_user', $id_user)->get();
+        $brg = Cart::join('barangs', 'barangs.id_barang', '=', 'carts.id_barang')->select('stok')->where('id_user', $id_user)->get();
         $validatorTransaksi = Validator::make($transaksi = $request->all(), [
-            // 'id_user' => ['required'],
             'no_order' => ['required'],
             'tanggal_order' => ['required'],
             'nama_penerima' => ['required'],
@@ -73,13 +138,8 @@ class TransaksiController extends Controller
         ]);
 
         $validatorDetail = Validator::make($detail = $request->all(), [
-            // 'id_user' => ['required'],
             'no_order' => ['required'],
-            'id_barang' => ['required'],
-            'qty' => ['required'],
-
         ]);
-
 
         if ($validatorDetail->fails() && $validatorTransaksi->fails()) {
             return response()->json(['status' => 400, 'errors' => $validatorTransaksi->errors(), 'detail_order' => $validatorDetail->errors()], 400);
@@ -88,8 +148,31 @@ class TransaksiController extends Controller
         } else if ($validatorTransaksi->fails()) {
             return response()->json(['status' => 400, 'errors' => $validatorTransaksi->errors()], 400);
         }
-
         try {
+            if ($checkcart == '[]') {
+                echo 'keranjang kosong';
+            }
+            foreach ($cart as $cart) {
+                if ($cart->qty > $cart->stok) {
+                    try {
+                        return response()->json(['status' => 422, 'message' => 'Melebihi Stok']);
+                    } catch (\Throwable $th) {
+                        return response()->json(['status' => 500, 'errors' => $th->getMessage()]);
+                    }
+                }
+                $id_barang = $cart->id_barang;
+                $detail = DetailOrder::create([
+                    'no_order' => $transaksi['no_order'],
+                    'id_user' => $id_user,
+                    // 'id_barang' => $id_barang,
+                    // 'qty' => $cart->qty
+                ]);
+                $carts = Barang::where('id_barang', $id_barang);
+                $kurang = $cart->stok - $cart->qty;
+                $carts->update(['stok' => $kurang]);
+                $check = DB::table('carts')->where('id_user', $id_user)->where('id_barang', $id_barang);
+                $check->delete();
+            }
 
             $barang = Transaksi::create([
                 'id_user' => $id_user,
@@ -103,26 +186,11 @@ class TransaksiController extends Controller
                 'total_bayar' => $request->total_bayar,
                 'status_bayar' => $request->status_bayar,
             ]);
-
-            $detail = DetailOrder::create([
-                'no_order' => $transaksi['no_order'],
-                'id_user' =>  $id_user,
-                'id_barang' => $request->id_barang,
-                'qty' => $request->qty,
-
-            ]);
-            $qtybarang = Barang::find($request->id_barang);
-            $qtybarangs = DB::table('barangs')->where('id_barang', $request->id_barang);
-            $qtyminus = ($qtybarang['stok'] - $request->qty);
-            $qtybarangs->update([
-                'stok' => $qtyminus
-            ]);
-            return response()->json(['status' => 200, 'meesage' => 'Transaksi berhasil ditambahkan', 'data'  =>  $barang, 'detail_order' => $detail], 200);
-        } catch (\Exception $e) {
-            return response()->json(['status' => 400, 'errors' => $e->getMessage()], 400);
+            return response()->json(['status' => 200, 'meesage' => 'Transaksi berhasil ditambahkan', 'data' => $barang, 'detail_order' => $detail, $lebih, $brg, $lebih > $brg], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['status' => 500, 'message' => $th->getMessage()]);
         }
     }
-
     /**
      * Display the specified resource.
      *
